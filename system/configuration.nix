60d9aa9c5fdd0de12590d35d4af64b3f61602ace
Martijn de Munnik <martijndemunnik@protonmail.com>
2023-09-08 17:14:05 +0200

    Wireguard via wg-quick

--- system/configuration.nix
+++ system/configuration.nix
@@ -36,36 +36,21 @@
   };
 
   # Enable WireGuard
-  networking.wireguard.interfaces = {
+  networking.wg-quick.interfaces = let
+    publicKey = "MhaYKOT9bs157g0qHin7nFg33zzpIzCk7O7Y0DbKJTo=";
+  in {
     # "wg0" is the network interface name. You can name the interface arbitrarily.
     wg0 = {
-      # Determines the IP address and subnet of the client's end of the tunnel interface.
-      ips = [ "10.24.0.10/16" ];
+      address = [ "10.24.0.10/16" ];
       listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
-
-      # Path to the private key file.
-      #
-      # Note: The private key can also be included inline via the privateKey option,
-      # but this makes the private key world-readable; thus, using privateKeyFile is
-      # recommended.
       privateKeyFile = "/etc/wireguard/privatekey";
+      postUp = ["wg set wg0 peer ${publicKey} persistent-keepalive 25"];
 
       peers = [
-        # For a client configuration, one peer entry for the server will suffice.
-
         {
-          # Public key of the server (not a file path).
-          publicKey = "MhaYKOT9bs157g0qHin7nFg33zzpIzCk7O7Y0DbKJTo=";
-
-          # Forward all the traffic via VPN.
+          inherit publicKey;
           allowedIPs = [ "10.24.0.0/16" ];
-          # Or forward only particular subnets
-          #allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
-
-          # Set this to the server IP and port.
           endpoint = "136.144.250.141:51820"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
-
-          # Send keepalives every 25 seconds. Important to keep NAT tables alive.
           persistentKeepalive = 25;
         }
       ];
@@ -238,7 +223,7 @@
   programs.dconf.enable = true;
   programs.zsh.enable = true;
 
-  #services.teamviewer.enable = true;
+  # services.teamviewer.enable = true;
   programs.thunar.enable = true;
   programs.thunar.plugins = with pkgs.xfce; [
     thunar-archive-plugin
